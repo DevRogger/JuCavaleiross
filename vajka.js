a4da37c05a48cce292ae21166c2fb89471421ab1
DevRogger <roggermano2016@gmail.com>
2026-01-06 07:18:12 -0300

    Initial commit

---
+++ vajka.js
@@ -0,0 +1,6 @@
+var bortao = document.querySelector('.icone')
+var conteudo = document.querySelector('.backmenu')
+
+bortao.addEventListener('click',()=>{
+    conteudo.classList.toggle('ativar')
+})
